680baa7d478e5bb4e25a3e7e660736ac70b127d9
cailanlf <40549567+cailanlf@users.noreply.github.com>
2025-11-04 10:12:16 -0500

    add more tests

--- test/test_packet.py
+++ test/test_packet.py
@@ -11,3 +11,30 @@ class TestPacket(TestCase):
 
         self.assertEqual(formatted, b"m,34923;")
         self.assertEqual(decoded, packet)
+
+    def test_format_decode_2(self):
+        packet = ['e', str(43023), str(2340)]
+
+        formatted = format_packet(packet)
+        decoded = split_packet(formatted)
+
+        self.assertEqual(formatted, b"e,43023,2340;")
+        self.assertEqual(decoded, packet)
+
+    def test_one_arg(self):
+        packet = ['t']
+
+        formatted = format_packet(packet)
+        decoded = split_packet(formatted)
+
+        self.assertEqual(formatted, b"t;")
+        self.assertEqual(decoded, packet)
+
+    def test_empty_args(self):
+        packet = ['t', '', '3']
+
+        formatted = format_packet(packet)
+        decoded = split_packet(formatted)
+
+        self.assertEqual(formatted, b"t,,3;")
+        self.assertEqual(decoded, packet)
